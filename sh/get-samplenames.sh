f17e00f9aa2e08ca2275a7081659757fa00dae96
Alexander Korsunsky <a.korsunsky@gmail.com>
2015-04-20 10:36:55 +0200

    Update samplenames directories

--- sh/get-samplenames.sh
+++ sh/get-samplenames.sh
@@ -3,18 +3,20 @@
 pushd () { command pushd "$@" > /dev/null; }
 popd () { command popd "$@" > /dev/null; }
 
+PREFIX=/export/bse/reads/old
+
 printf '# 454 samples\n'
-pushd /export/bse/454
+pushd $PREFIX/454
 get-samplenames-from-454-dir.sh
 popd
 
 printf '# illumina samples\n'
-pushd /export/bse/paul
+pushd $PREFIX/paul
 get-samplenames-from-illumina-dir.sh
 popd
 
 printf '# amprolium samples\n'
-pushd /export/bse/illumina
+pushd $PREFIX/illumina
 get-samplenames-from-amprolium-dir.sh
 popd
 
