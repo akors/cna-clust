3ae416d47810b0334cd4d75e0f07e7f71587c714
Alexander Korsunsky <a.korsunsky@gmail.com>
2015-06-08 11:35:07 +0200

    py: add find-mountpoint script

---
+++ py/find-mountpoint.py
@@ -0,0 +1,16 @@
+#!/usr/local/bin/python3
+
+import sys, os
+
+def find_mount_point(path):
+    path = os.path.abspath(path)
+    while not os.path.ismount(path):
+        path = os.path.dirname(path)
+        return path
+
+if __name__ == "__main__":
+    mountpoints = {p : find_mount_point(p) for p in sys.argv[1:]}
+
+    for k in mountpoints:
+       print("{}:\t{}".format(k, mountpoints[k]))
+
