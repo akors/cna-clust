1c53b9a285cda35687f20c1419e1acaba0a1ca2b
Alexander Korsunsky <a.korsunsky@gmail.com>
2015-08-03 11:55:25 +0200

    find-mountpoint: fix warnings

--- py/find-mountpoint.py
+++ py/find-mountpoint.py
@@ -2,6 +2,7 @@
 
 import sys, os
 
+
 def find_mount_point(path):
     path = os.path.abspath(path)
     while not os.path.ismount(path):
@@ -9,8 +10,8 @@ def find_mount_point(path):
         return path
 
 if __name__ == "__main__":
-    mountpoints = {p : find_mount_point(p) for p in sys.argv[1:]}
+    mountpoints = {p: find_mount_point(p) for p in sys.argv[1:]}
 
     for k in mountpoints:
-       print("{}:\t{}".format(k, mountpoints[k]))
+        print("{}:\t{}".format(k, mountpoints[k]))
 
