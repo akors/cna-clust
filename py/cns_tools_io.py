f526f2d2297a715b3f96530cf66fd1f8d8c55e9d
Alexander Korsunsky <a.korsunsky@gmail.com>
2015-08-05 13:35:18 +0200

    cns_tools_io: make command line invocation consistent

--- py/cns_tools_io.py
+++ py/cns_tools_io.py
@@ -1,61 +1,61 @@
-
+#!/usr/local/bin/python3
 # (C) 2015, Alexander Korsunsky
 
-import sys, io
-import logging, configparser
-
+import logging
 
 import time
+import io
 import re
 import numpy as np
-
 
 LOGDEFAULT = logging.DEBUG
 logger = logging.getLogger(__name__)
 
 
-def get_shape(filename):
-    rows = 0
-    cols = 0
+def get_shape(infile):
+    assert isinstance(infile, io.IOBase)
+    
+    # store old file pointer
+    old_pos = infile.tell()
 
-    with open(filename, 'rt') as fd:
+    # seek to the beginning, skip through liness
+    infile.seek(0, io.SEEK_SET)
 
-        count_starttime = time.time()
-        for line in fd:
-            rows += 1
+    count_starttime = time.time()
 
-        cols = len(line.split("\t"))
+    cols = len(next(infile).split("\t"))
 
-        logger.debug("Line counting took %.3f s", time.time() - count_starttime)
+    # count lines
+    rows = 1 + sum(1 for _ in infile)
 
+    logger.debug("Line counting took %.3f s", time.time() - count_starttime)
+
+    infile.seek(old_pos, io.SEEK_SET)
     return rows, cols
 
 
-def read_cuffcmp_tracking(filename):
-    starttime = time.time()
+def read_cuffcmp_tracking(infile):
+    # starttime = time.time()
 
-    linecount = get_shape(filename)[0]
+    linecount = get_shape(infile)[0]
 
     tracking_data = [None for x in range(1, linecount + 1)]
 
     parse_starttime = time.time()
-    tracking_data[:] = lineit_cuffcmp_tracking(filename)
-
+    tracking_data[:] = lineit_cuffcmp_tracking(infile)
 
     logger.debug("Parsing took %.3f s", time.time() - parse_starttime)
 
     return tracking_data
 
 
-def lineit_cuffcmp_tracking(filename):
-
-    with open(filename, 'rt') as fd:
-        for line in fd:
-#            try:
+def lineit_cuffcmp_tracking(infile):
+    for line in infile:
+        # try:
             yield parseline_cuffcmp_tracking(line)
-#            except:
-#                logger.verbose("Invalide line in input file")
-#                continue
+        # except:
+        #     logger.verbose("Invalide line in input file")
+        #     continue
 
 
 def parseline_cuffcmp_tracking(line):
@@ -68,6 +68,8 @@ def parseline_cuffcmp_tracking(line):
 
 # re_sampleid = re.compile('q(\d+)')
 re_sample_transcripts = re.compile('^q\d+:(.*)$')
+
+
 def parse_transcripts_cuffcmp_tracking(sampe_transcripts):
     # 0: <gene_id>
     # 1: <transcript_id>
@@ -78,21 +80,18 @@ def parse_transcripts_cuffcmp_tracking(sampe_transcripts):
     # 6: <cov>
     # # 7: <len> No len, because some transcripts don't have length information for some reason
 
-
     # sampe_transcripts = sampe_transcripts.strip()
 
     if sampe_transcripts[0] == "-":
         return None
 
-
     ts = re_sample_transcripts.match(sampe_transcripts).group(1)
     trascript_grouplist = [t.split("|") for t in ts.split(",")]
 
     return [
-        (g[0], g[1], float(g[2]), float(g[3]), float(g[4]), float(g[5]), float(g[6]))#, int(g[7])
+        (g[0], g[1], float(g[2]), float(g[3]), float(g[4]), float(g[5]), float(g[6]))  # , int(g[7])
         for g in trascript_grouplist
     ]
-
 
 
 def binmatrix_from_cuffcmp_tracking(filedata):
@@ -103,7 +102,7 @@ def binmatrix_from_cuffcmp_tracking(filedata):
     starttime = time.time()
 
     # extract binary matrix
-    binmatrix = np.array(filedata[:,4:], dtype=np.bool)
+    binmatrix = np.array(filedata[:, 4:], dtype=np.bool)
 
     # tock
     logger.debug("Creating binary matrix took %.3f s", time.time() - starttime)
@@ -114,8 +113,6 @@ def binmatrix_from_cuffcmp_tracking(filedata):
 def fpkmmatrix_from_cuffcmp_tracking(filedata):
     r = len(filedata)
     c = len(filedata[0])
-
-    #print("r = %d, c = %d\n" % (r ,c))
 
     # tick
     starttime = time.time()
@@ -136,15 +133,69 @@ def fpkmmatrix_from_cuffcmp_tracking(filedata):
 
 
 if __name__ == "__main__":
-    import time, sys
+    import time, sys, argparse
 
-    logging.basicConfig(level=LOGDEFAULT, format='%(levelname)1s:%(message)s')
+    def main_read_cuffmp(args, parser):
+        filedata = read_cuffcmp_tracking(args.infile)
 
-    for arg in sys.argv[1:]:
-        filedata = read_cuffcmp_tracking(arg)
-        fpkmmatrix = fpkmmatrix_from_cuffcmp_tracking(filedata)
-
+        if args.format == 'fpkm':
+            data = fpkmmatrix_from_cuffcmp_tracking(filedata)
+        elif args.format == 'fpkm':
+            data = binmatrix_from_cuffcmp_tracking(filedata)
+        else:
+            raise AssertionError("Unknown output format `" + args.format + "`")
 
         starttime = time.time()
-        np.save(arg+".fpkmmatrix", fpkmmatrix)
+        np.save(args.outfile, data)
         logger.debug("Saving uncompressed took %.3f s", time.time() - starttime)
+
+
+    parser_top = argparse.ArgumentParser(description='CNS Data IO')
+    parser_top.add_argument('--log_level', action="store",
+                            type=str.upper, dest='log_level',
+                            metavar='LOG_LEVEL',
+                            choices=("DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"),
+                            default=LOGDEFAULT,
+                            help='Set log level to be LOG_LEVEL. Can be one of: DEBUG,INFO,WARNING,ERROR,CRITICAL')
+
+    subparsers = parser_top.add_subparsers(title='Actions', description='Available IO operations',
+                                           dest='main_action')
+
+    parser_read_cuffcmp = subparsers.add_parser('read-cuffcmp',
+                                                help='Parse a cuffcmp.tracking file and write the data as '
+                                                     'a numpy matrix.')
+
+    parser_read_cuffcmp.add_argument('infile', action="store",
+                                     type=argparse.FileType(mode='rt'),
+                                     metavar='INFILE',
+                                     help='cuffcmp.tracking input file')
+
+    parser_read_cuffcmp.add_argument('outfile', action="store",
+                                 type=argparse.FileType(mode='wb'),
+                                 metavar='OUTFILE',
+                                 help='Output file')
+
+    parser_read_cuffcmp.add_argument('-f', '--format', action="store",
+                                     type=str, dest='format',
+                                     choices=('fpkm', 'bin'), required=True,
+                                     metavar='FORMAT',
+                                     help='Write output as FORMAT, where FORMAT is one of:\n'
+                                          'fpkm: FPKM only matrix; \n'
+                                          'bin:  Binary only matrix\n')
+
+    args = parser_top.parse_args()
+
+    logging.basicConfig(level=args.log_level, format='%(levelname)1s:%(message)s')
+
+    if not args.main_action:
+        parser_top.error('No action selected')
+    elif args.main_action == 'read-cuffcmp':
+        main_read_cuffmp(args, parser_read_cuffcmp)
+
+    # for arg in sys.argv[1:]:
+    #     filedata = read_cuffcmp_tracking(arg)
+    #     fpkmmatrix = fpkmmatrix_from_cuffcmp_tracking(filedata)
+    #
+    #     starttime = time.time()
+    #     np.save(arg+".fpkmmatrix", fpkmmatrix)
+    #     logger.debug("Saving uncompressed took %.3f s", time.time() - starttime)
